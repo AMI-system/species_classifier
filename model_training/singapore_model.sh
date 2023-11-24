6919a39cdbf900407dc605414067769a76084c04
KatrionaGoldmann <kgoldmann@turing.ac.uk>
2023-11-24 12:12:59 +0000

    Create singapore model

--- model_training/singapore_model.sh
+++ model_training/singapore_model.sh
@@ -23,35 +23,51 @@ conda activate ~/amber/kg_conda_env2
 #/bask/homes/f/fspo1218/NVIDIA_CUDA-11.1_Samples/bin/x86_64/linux/release/cudaOpenMP
 
 # 1. create_dataset_split
-echo 'Create dataset split'
-python 01_create_dataset_split.py \
-    --data_dir /bask/homes/f/fspo1218/amber/data/gbif_macro_data/gbif_macro/ \
-    --write_dir /bask/homes/f/fspo1218/amber/data/gbif_macro_data/ \
-    --species_list /bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv \
-    --train_ratio 0.75 \
-    --val_ratio 0.10 \
-    --test_ratio 0.15 \
-    --filename 01_singapore_macro_data
+# echo 'Create dataset split'
+# python 01_create_dataset_split.py \
+#     --data_dir /bask/homes/f/fspo1218/amber/data/gbif_download_standalone/gbif_images/ \
+#     --write_dir /bask/homes/f/fspo1218/amber/data/gbif_singapore/ \
+#     --species_list /bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv \
+#     --train_ratio 0.75 \
+#     --val_ratio 0.10 \
+#     --test_ratio 0.15 \
+#     --filename 01_singapore_data
 
 
 
 # 2. calculate_taxa_statistics
-python 02_calculate_taxa_statistics.py \
-    --species_list /bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv \
-    --write_dir /bask/homes/f/fspo1218/amber/data/gbif_macro_data/ \
-    --numeric_labels_filename 01_uk_macro_data_numeric_labels \
-    --taxon_hierarchy_filename 01_uk_macro_data_taxon_hierarchy \
-    --training_points_filename 01_uk_macro_data_count_training_points \
-    --train_split_file /bask/homes/f/fspo1218/amber/data/gbif_macro_data/01_singapore_macro_data-train-split.csv
+# python 02_calculate_taxa_statistics.py \
+#     --species_list /bask/homes/f/fspo1218/amber/projects/gbif_download_standalone/species_checklists/singapore-moths-keys-nodup.csv \
+#     --write_dir /bask/homes/f/fspo1218/amber/data/gbif_singapore/ \
+#     --numeric_labels_filename 01_singapore_data_numeric_labels \
+#     --taxon_hierarchy_filename 01_singapore_data_taxon_hierarchy \
+#     --training_points_filename 01_singapore_data_count_training_points \
+#     --train_split_file /bask/homes/f/fspo1218/amber/data/gbif_singapore/01_singapore_data-train-split.csv
 
-# 5. Train the model
-# python 04_train_model.py  \
-#     --train_webdataset_url "/bask/projects/v/vjgo8416-amber/data/binary_moth_training/gbif_crop_ready/datasets/macro/train/train-500-{000000..000009}.tar" \
-#     --val_webdataset_url "/bask/projects/v/vjgo8416-amber/data/binary_moth_training/gbif_crop_ready/datasets/macro/val/val-500-{000000..000001}.tar" \
-#     --test_webdataset_url "/bask/projects/v/vjgo8416-amber/data/binary_moth_training/gbif_crop_ready/datasets/macro/test/test-500-{000000..000001}.tar" \
-#     --config_file ./configs/01_uk_macro_data_config_cropped.json \
-#     --dataloader_num_workers 6 \
-#     --random_seed 42
+# 3. create webdataset
+# for VARIABLE in 'train' 'val' 'test'
+# do
+#     echo '--' $VARIABLE
+#     mkdir -p /bask/homes/f/fspo1218/amber/data/gbif_singapore/$VARIABLE
+#     python 03_create_webdataset.py \
+#         --dataset_dir /bask/homes/f/fspo1218/amber/data/gbif_download_standalone/gbif_images/ \
+#         --dataset_filepath /bask/homes/f/fspo1218/amber/data/gbif_singapore/01_singapore_data-$VARIABLE-split.csv \
+#         --label_filepath /bask/homes/f/fspo1218/amber/data/gbif_singapore/01_singapore_data_numeric_labels.json \
+#         --image_resize 500 \
+#         --max_shard_size 100000000 \
+#         --webdataset_pattern "/bask/homes/f/fspo1218/amber/data/gbif_singapore/$VARIABLE/$VARIABLE-500-%06d.tar"
+# done
+
+
+# 4. Train the model
+echo 'Training the model'
+python 04_train_model.py  \
+    --train_webdataset_url "/bask/homes/f/fspo1218/amber/data/gbif_singapore/train/train-500-{000000..000160}.tar" \
+    --val_webdataset_url "/bask/homes/f/fspo1218/amber/data/gbif_singapore/val/val-500-{000000..000021}.tar" \
+    --test_webdataset_url "/bask/homes/f/fspo1218/amber/data/gbif_singapore/test/test-500-{000000..000031}.tar" \
+    --config_file ./configs/01_singapore_data_config.json \
+    --dataloader_num_workers 6 \
+    --random_seed 42
 
 # ding ding
 echo $'\a'
